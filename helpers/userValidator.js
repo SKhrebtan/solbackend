2f05fb0a9d57ce656a8d0f2166214ff50b14422f
123 <mac@Macs-Mac-mini.local>
2023-12-15 16:52:04 +0200

    fix number

--- helpers/userValidator.js
+++ helpers/userValidator.js
@@ -4,5 +4,5 @@ exports.createUserDataValidator = (data) =>
   Joi.object().options({ abortEarly: false }).keys({
     name: Joi.string().min(2).max(30).required(),
     email: Joi.string().email().required(),
-    number: Joi.number().regex(/^\+?3?8?(0[5-9][0-9]\d{7})$/).required(),
+    phone: Joi.regex(/^\+?3?8?(0[5-9][0-9]\d{7})$/).required(),
   }).validate(data);
