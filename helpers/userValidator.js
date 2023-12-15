802b4d232711d39af562952e44ca1607fe88f073
123 <mac@Macs-Mac-mini.local>
2023-12-15 17:05:28 +0200

    fix uri

--- helpers/userValidator.js
+++ helpers/userValidator.js
@@ -1,8 +1,9 @@
 const Joi = require('joi');
 
+const phoneRegex = /^\+?3?8?(0[5-9][0-9]\d{7})$/;
 exports.createUserDataValidator = (data) =>
   Joi.object().options({ abortEarly: false }).keys({
     name: Joi.string().min(2).max(30).required(),
     email: Joi.string().email().required(),
-    phone: Joi.regex(/^\+?3?8?(0[5-9][0-9]\d{7})$/).required(),
+    phone: Joi.string().regex(phoneRegex).required(),
   }).validate(data);
